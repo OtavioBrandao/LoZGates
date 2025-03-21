a7f571590b520287c9fa06c5df8cfb7b79145a78
Larissa <larissafdds@gmail.com>
2025-03-21 16:46:18 -0300

    Update karnaugh.py

--- LoZGates 1.0.1/karnaugh.py
+++ LoZGates 1.0.1/karnaugh.py
@@ -1,59 +1,87 @@
 from itertools import product
 
-def get_minterms(expressao, variables):
+def pegar_termos(expressao, variaveis):
     minterms = []
-    n = len(variables)
+    n = len(variaveis)
+    expressao_modificada = expressao.replace("!", "not ").replace("|", " or ").replace(">", "<=").replace("&", " and ")
+    
     for values in product([0, 1], repeat=n):
-        assignment = dict(zip(variables, values))
-        if eval(expressao, assignment):
+        assignment = dict(zip(variaveis, values))
+        if eval(expressao_modificada, assignment):
             minterms.append(values)
     return minterms
 
-def generate_karnaugh_map(minterms, variables):
-    n = len(variables)
+def gerar_mapa_karnaugh(minterms, variaveis):
+    n = len(variaveis)
     mapa = ""
+    
     if n == 2:
-        mapa += "Mapa de Karnaugh para 2 variáveis:\n"
-        mapa += "  0 1\n"
+        mapa += "\n"
+        mapa += f"Mapa de Karnaugh para 2 variáveis:\n"
+        mapa += "\n"
+        mapa += f"   {variaveis[1]}'     {variaveis[1]}\n"
+        mapa += "     +-------+-------+\n"
+        
         for i in range(2):
-            row = f"{i}|"
+            row = f"{variaveis[0]}'" if i == 0 else f"{variaveis[0]}"
+            row += " |"
             for j in range(2):
                 if (i, j) in minterms:
-                    row += "1 "
+                    row += "   1   |"
                 else:
-                    row += "0 "
+                    row += "   0   |"
             mapa += row + "\n"
+            mapa += "     +-------+-------+\n"
+    
     elif n == 3:
-        mapa += "Mapa de Karnaugh para 3 variáveis:\n"
-        mapa += "  00 01 11 10\n"
+        mapa += f"Mapa de Karnaugh para 3 variáveis:\n"
+        mapa += "\n"
+        mapa += f"      {variaveis[1]}'{variaveis[2]}'   {variaveis[1]}'{variaveis[2]}   {variaveis[1]}{variaveis[2]}   {variaveis[1]}{variaveis[2]}'\n"
+        mapa += "     +-------+-------+-------+-------+\n"
+        
         for i in range(2):
-            row = f"{i}|"
+            row = f"{variaveis[0]}'" if i == 0 else f"{variaveis[0]}"
+            row += " |"
             for j in range(4):
                 if (i, j // 2, j % 2) in minterms:
-                    row += "1 "
+                    row += "   1   |"
                 else:
-                    row += "0 "
+                    row += "   0   |"
             mapa += row + "\n"
+            mapa += "     +-------+-------+-------+-------+\n"
+    
     elif n == 4:
-        mapa += "Mapa de Karnaugh para 4 variáveis:\n"
-        mapa += "  00 01 11 10\n"
-        for i in range(4):
-            row = f"{i}|"
-            for j in range(4):
-                if (i // 2, i % 2, j // 2, j % 2) in minterms:
-                    row += "1 "
+        mapa += f"Mapa de Karnaugh para 4 variáveis:\n"
+        mapa += "\n"
+        mapa += f"      {variaveis[2]}'{variaveis[3]}'   {variaveis[2]}'{variaveis[3]}   {variaveis[2]}{variaveis[3]}   {variaveis[2]}{variaveis[3]}'\n"
+        mapa += "     +-------+-------+-------+-------+\n"
+        
+        for i in [0, 1, 3, 2]:
+            row = f"{variaveis[0]}'{variaveis[1]}'" if i == 0 else \
+                f"{variaveis[0]}'{variaveis[1]}" if i == 1 else \
+                f"{variaveis[0]}{variaveis[1]}" if i == 3 else \
+                f"{variaveis[0]}{variaveis[1]}'"
+            row += " |"
+    
+            for j in [0, 1, 3, 2]:  
+                p = i // 2
+                q = i % 2
+                r = j // 2
+                t = j % 2
+                if (p, q, r, t) in minterms:
+                    row += "   1   |"
                 else:
-                    row += "0 "
+                    row += "   0   |"
             mapa += row + "\n"
+            mapa += "     +-------+-------+-------+-------+\n"
+    
     else:
         mapa += "Número de variáveis não suportado.\n"
     return mapa
 
-def karnaugh_map(expressao, variables):
-    # Substitui a implicação pela sua forma equivalente
-    expressao_modificada = expressao.replace(">", "<=")
-    minterms = get_minterms(expressao_modificada, variables)
-    return generate_karnaugh_map(minterms, variables)
+def karnaugh_map(expressao, variaveis):
+    minterms = pegar_termos(expressao, variaveis)
+    return gerar_mapa_karnaugh(minterms, variaveis)
     
 def analisar(expressao):
     variaveis = set()
