2fbf85b46abc761ba9fbeee3fc81c9ea839873bb
LarissaFDS <larissafdds@gmail.com>
2025-07-15 10:18:57 -0300

    otavio

--- LoZGates 1.0.1/FrontEnd/interface.py
+++ LoZGates 1.0.1/FrontEnd/interface.py
@@ -512,7 +512,7 @@ def inicializar_interface():
 
         entrada_txt = entrada.get().strip().upper()
         if not entrada_txt:
-            popup_erro("Digite uma expressão primeiro.")
+            popup_erro("zil é besta demais.")
             return
 
         saida_booleana = converter_para_algebra_booleana(entrada_txt)
