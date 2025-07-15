8736b4d1bde417c44b24d0ed4b121636fbb4890f
Otavio <otaviomenezes574@gmail.com>
2025-07-15 10:08:44 -0300

    teste

--- LoZGates 1.0.1/FrontEnd/interface.py
+++ LoZGates 1.0.1/FrontEnd/interface.py
@@ -461,7 +461,7 @@ def inicializar_interface():
                 img = Image.open(caminho_img)
                 img.save(caminho_salvar)
         else:
-            popup_erro("Imagem não encontrad.")
+            popup_erro("Imagem não encontradaaa.")
 
     botao_salvar = ctk.CTkButton(
         scroll_frame1,
